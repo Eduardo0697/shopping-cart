471db685af391185e88f89ee72b629a1353f3405
Eduardo Hernandez <eduardo0697@armandoschmitt.de>
2021-09-28 13:42:38 -0500

    Show the shopping cart and calculate the total

--- src/store/index.js
+++ src/store/index.js
@@ -16,6 +16,24 @@ export default new Vuex.Store({
     // Getter track dependencies and change when a dependency change
     availableProducts(state, getters){ // getters are all the existing getters
       return state.products.filter((product => product.inventory > 0))
+    },
+    cartProducts(state){
+      return state.cart.map(cartItem => {
+        const product = state.products.find(product => product.id === cartItem.id)
+        return{
+          title: product.title,
+          price: product.price,
+          quantity: cartItem.quantity
+        }
+      })
+    },
+    cartTotal(state, getters){
+      // let total = 0
+      // getters.cartProducts.forEach(product => {
+      //   total += product.price * product.quantity
+      // });
+      // return total
+      return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
     }
   },
 
